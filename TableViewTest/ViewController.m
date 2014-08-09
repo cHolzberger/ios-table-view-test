6ae49df07ae642e73f6ac0e1e8aa34d6a61a99d6
Christian Holzberger <ch@mosaik-software.de>
2014-08-09 16:22:00 +0200

    Dynamic Height of Labels

--- TableViewTest/ViewController.m
+++ TableViewTest/ViewController.m
@@ -13,12 +13,18 @@
 @end
 
 @implementation ViewController
-UILabel *footer;
-UIView *footerView;
+
 CGSize s;
+static NSArray* labels;
+
++ (void)initialize {
+          labels = [NSArray arrayWithObjects: @"1.", @"2.", @"Read something about this app. Read something about this app. Read something about this app. Read something about this app. Read something about this app. Read something about this app. Read something about this app. Read something about this app.",nil];
+}
+
 - (void)viewDidLoad
 {
     [super viewDidLoad];
+  
 	// Do any additional setup after loading the view, typically from a nib.
 }
 
@@ -37,7 +43,7 @@ CGSize s;
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
 {
-    return 2;
+    return labels.count;
 }
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
@@ -48,23 +54,35 @@ CGSize s;
 }
 
 - (UIView*)tableView:(UITableView *)tableView viewForHeaderInSection:(NSInteger)section{
+    UILabel *footer;
+    UIView *footerView;
+    NSString* text = labels[section];
+
+    CGRect labelRect = [text
+                        boundingRectWithSize:CGSizeMake(tableView.frame.size.width-20, 500)
+                        options:NSStringDrawingUsesLineFragmentOrigin
+                        attributes:@{
+                                     NSFontAttributeName : [UIFont systemFontOfSize:14]
+                                     }
+                        context:nil];
     
     footerView = [[UIView alloc] init];
-    footer = [[UILabel alloc] initWithFrame:CGRectMake(10, 0, tableView.frame.size.width-20, 120)];
+    footer = [[UILabel alloc] initWithFrame:CGRectMake(10, 0, labelRect.size.width, labelRect.size.height)];
     [footer setNumberOfLines:0];
     [footer setLineBreakMode:NSLineBreakByWordWrapping];
-    [footer setText:NSLocalizedString(@"Read something about this app. Read something about this app. Read something about this app. Read something about this app. Read something about this app. Read something about this app. Read something about this app. Read something about this app.",nil)];
+    [footer setText:NSLocalizedString(text,nil)];
     
     [footerView addSubview:footer];
     s = [footer sizeThatFits: CGSizeMake(tableView.frame.size.width-20, 90)];
+    footer.font = [UIFont systemFontOfSize:14];
     return footerView;
 }
 
 -(CGFloat)tableView:(UITableView *)tableView heightForHeaderInSection:(NSInteger)section{
-    NSString* text = @"Read something about this app. Read something about this app. Read something about this app. Read something about this app. Read something about this app. Read something about this app. Read something about this app. Read something about this app.";
+    NSString* text = labels[section];
 
     CGRect labelRect = [text
-                        boundingRectWithSize:CGSizeMake(tableView.frame.size.width-20, 120)
+                        boundingRectWithSize:CGSizeMake(tableView.frame.size.width-20, 500)
                         options:NSStringDrawingUsesLineFragmentOrigin
                         attributes:@{
                                      NSFontAttributeName : [UIFont systemFontOfSize:14]
